cbf24477869e44eb97f835dd44c720e91a7d8cfe
guiltylord <desxzawq@gmail.com>
2026-02-19 15:30:29 +0300

    refactor!: onion style

--- src/services/clean_data.py
+++ src/services/clean_data.py
@@ -1,8 +1,10 @@
 import re
 
+
 def clean_text(text: str) -> str:
     text = re.sub(r"\s+", " ", text)
     text = re.sub(r"[^\x00-\x7F]+", " ", text)
     return text.strip()
+
 
 print(clean_text("Hello, world!"))
