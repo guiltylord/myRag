c3714a0bc4a33a3ac87c4929cc4022439ec3cf12
guiltylord <desxzawq@gmail.com>
2026-02-19 17:42:06 +0300

    feat: upgrade split docs
    add more splitters, change chunk size and overlap

--- src/services/split_data.py
+++ src/services/split_data.py
@@ -1,9 +1,22 @@
-def split_docs(prepared_docs, chunk_size=500, chunk_overlap=100):
-    from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def split_docs(prepared_docs, chunk_size=300, chunk_overlap=50):
+    """
+    Разбивает список текстов на чанки.
+    
+    Args:
+        prepared_docs (list[str]): Список строк (текстов).
+        chunk_size (int): Размер одного чанка в символах.
+        chunk_overlap (int): Сколько символов захватывать из предыдущего чанка (контекст).
+    """
+    
 
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size, chunk_overlap=chunk_overlap
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        separators=["\n\n", "\n", ". ", " ", ""]
     )
+    
     chunks = splitter.create_documents(prepared_docs)
-    print(f"Total chunks created: {len(chunks)}")
+    print(f"✅ Разбиение завершено. Исходных доков: {len(prepared_docs)}, создано чанков: {len(chunks)}")
     return chunks
