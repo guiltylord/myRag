153d4544d9c2bd255d4b3f60adde9505b414f71a
guiltylord <desxzawq@gmail.com>
2026-02-19 13:55:46 +0300

    refactor: split_data

--- split_data.py
+++ split_data.py
@@ -1,10 +1,10 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
-def split_docs(documents, chunk_size=500, chunk_overlap=100):
+def split_docs(prepared_docs, chunk_size=500, chunk_overlap=100):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size, chunk_overlap=chunk_overlap
     )
-    chunks = splitter.create_documents(documents)
+    chunks = splitter.create_documents(prepared_docs)
     print(f"Total chunks created: {len(chunks)}")
     return chunks
